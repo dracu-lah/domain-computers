b07b4cbb7a59cce5d8cea302f89a99ef62c252db
dracu-lah <nevilnicks4321@gmail.com>
2026-01-07 17:05:42 +0530

    fix:updated sitempa

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.domaincomputers.in',
+      url: 'https://domaincomputers.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
